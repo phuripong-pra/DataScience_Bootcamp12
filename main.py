1eb22d8a0ff00519504d30b395309c2a47eacde0
phuripong-pra <phuripong.pra@gmail.com>
2026-01-03 14:46:59 +0000

    Add a new line in main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 #print("Hello World from Peem!")
 print("Hello World from Peem!")
+
 #print("Goodbye World from Peem!")
 print("Goodbye World from Peem!")
+
+#print a new line
+print("I'm learning how to use github!")
