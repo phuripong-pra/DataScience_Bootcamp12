96519b002bbc10ec3edea8acb387b04bfd4f972e
phuripong-pra <phuripong.pra@gmail.com>
2026-01-03 14:54:15 +0000

    add add_two function

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ print("Goodbye World from Peem!")
 
 #print a new line
 print("I'm learning how to use github!")
+
+#new function
+def add_two(num):
+    return num + 2
+
+print(add_two(5), add_two(10))
